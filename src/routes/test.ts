e1bbd1eb2298ef52d23882e0f869df036c490dad
Kunal gaur <kunal.gaur@simplyfi.tech>
2023-12-18 13:34:28 +0530

    Added new test routes *Dont use in production*

---
+++ src/routes/test.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const router = Router();
+
+{/*
+*
+*
+*
+Please make sure to be cautious while using these endpoints. The endpoints are not for production and are purely for development purposes.
+*
+*
+*
+*/}
+
+export default router;
