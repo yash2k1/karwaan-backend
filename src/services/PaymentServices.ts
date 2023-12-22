f1fe188b6996053fe05d5f67e490756c411204d1
Kunal gaur <kunal.gaur@simplyfi.tech>
2023-12-22 17:55:17 +0530

    Added sdk (to be fixed)

--- src/services/PaymentServices.ts
+++ src/services/PaymentServices.ts
@@ -1,29 +1,30 @@
-import axios from "axios"
+import axios from "axios";
+import Logger from "../utils/Logger";
+import Razorpay from "razorpay";
+
+const razorpayInstance = new Razorpay({ 
+    key_id: `${process.env.RAZOR_PAY_KEY_ID}`, 
+    key_secret: `${process.env.RAZOR_PAY_SECRET_KEY}`,
+});
 
 export class PaymentServices {
-    static async createOrder (amount: number, order_id: string) {
+    static async createOrder(amount: number, order_id: string) {
         try {
-            const options = {
-                method: 'POST',
-                url: 'https://api.razorpay.com/v1/orders',
-                headers: {
-                    accept: 'application/json',
-                    'content/type': 'application/json',
-                    'x-key-id': process.env.RAZOR_PAY_KEY_ID,
-                    'x-key-secret': process.env.RAZOR_PAY_SECRET_KEY
-                },
-                data: {
-                    amount: amount,
-                    currency: 'INR',
-                    receipt: order_id,
-                    partial_payment: false
+            const order = await razorpayInstance.orders.create({
+                amount: amount,
+                currency: 'INR',
+                receipt: order_id,
+                partial_payment: false,
+                notes: {
+                    key1: "Order generation for karwaan films.",
+                    key2: null
                 }
-            } 
-
-            const res = await axios(options);
-            return res?.data;
-        } catch (error) {
-            throw error
-        }        
+            });
+            
+            return order;
+        } catch (error: any) {
+            // Log the error description
+            return Logger.error(error.description);
+        }
     }
 }
