b54b9ef1739d41cc727d8b25cb3d660d31ef00b4
Kunal gaur <kunal.gaur@simplyfi.tech>
2023-12-22 17:56:51 +0530

    Added validations (to be removed)

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -1,12 +1,22 @@
+import axios from "axios";
 import { NextFunction, Request, Response } from "express";
 
 export const globalErrorHandler = (error: any, _request: Request, response: Response, next:NextFunction) => {
     if(error){
+        if(axios.isAxiosError(error)){
+            return response.status(500).json({
+                status: "error",
+                statusCode: 500,
+                message: error.message || "An unknown error occured",
+                data: error.response?.data
+            });
+        };
+
         return response.status(500).json({
             status: "error",
             statusCode: 500,
             message: error.message || "An unknown error occured",
             data: null
-        })
+        });
     }
 }
