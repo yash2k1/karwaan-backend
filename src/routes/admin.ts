c46e5897235877dcb2269bc8957c3e91c6d4da1b
Kunal gaur <kunal.gaur@simplyfi.tech>
2023-12-22 17:56:31 +0530

    Added admin routes

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { addProduct, deleteProduct, updateProduct } from "../controller/admin";
+import { addProduct, deleteProduct, getAllCustomer, getSingleCustomer, updateProduct } from "../controller/admin";
 
 const router = Router();
 
 router.route('/create-product').post(addProduct);
 router.route('/update-product/:id').put(updateProduct);
 router.route('/delete-product/:id').delete(deleteProduct);
+router.route('/customer_details').get(getAllCustomer);
+router.route('/customer_detail/:id').get(getSingleCustomer);
+router.route('/revenue-generated').get(getSingleCustomer);
 
 export default router;
