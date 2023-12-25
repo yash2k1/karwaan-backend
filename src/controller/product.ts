cc666b342fa104766ed818aca08d17f47cab7f96
Kunal gaur <kunal.gaur@simplyfi.tech>
2023-12-25 18:38:56 +0530

    Refactored code

--- src/controller/product.ts
+++ src/controller/product.ts
@@ -5,11 +5,55 @@ import Product from "../model/product";
 import { Types, isObjectIdOrHexString } from "mongoose";
 import { ResponseData } from "../utils/ResponseData";
 
-export const getAllProducts = errorHandler(async (_request: Request, response: Response) => {
-    const data = await ProductServices.getAllProducts();
+export const getAllProducts = errorHandler(async (request: Request, response: Response) => {
+    let data: ResponseData;
+    let products;
 
+    const type = request.query.type as string;
+    const tag = request.query.tag as string;
+    const searchQuery = request.query.q as string;
+
+    if (type) {
+        if (type !== 'image' && type !== 'video') {
+            data = new ResponseData("error", 400, `${type} is not a valid type`, null);
+            return response.status(data.statusCode).json(data);
+        }
+
+        products = await Product.find({ 'media.type': type });
+        data = new ResponseData("success", 200, "Success", products);
+        return response.status(data.statusCode).json(data);
+    }
+
+    if (tag) {
+        const validTags = ['landscape', 'cityscape', 'dark', 'people', 'uncategorized'];
+
+        if (!validTags.includes(tag)) {
+            data = new ResponseData("error", 400, `${tag} is not a valid tag.`, null);
+            return response.status(data.statusCode).json(data);
+        }
+
+        products = await Product.find({ tags: { $in: [tag] } });
+        data = new ResponseData("success", 200, "Success", products);
+        return response.status(data.statusCode).json(data);
+    }
+
+    if (searchQuery) {
+            products = await Product.find({
+                $or: [
+                    { name: { $regex: searchQuery, $options: 'i' } },
+                    { description: { $regex: searchQuery, $options: 'i' } },
+                ]
+            });
+
+            data = new ResponseData("success", 200, "Success", products);
+            return response.status(data.statusCode).json(data);
+        }
+
+    products = await Product.find();
+    data = new ResponseData("success", 200, "Success", products);
     return response.status(data.statusCode).json(data);
 });
+
 
 export const getSingleProduct= errorHandler(async (request: Request, response: Response) => {
     let data;
@@ -23,22 +67,3 @@ export const getSingleProduct= errorHandler(async (request: Request, response: R
     data = await ProductServices.getSingleProduct(productId);
     return response.status(data.statusCode).json(data);
 });
-
-export const searchProducts = errorHandler(async (request:Request, response: Response) => {
-    const { tags, name } = request.query;
-    const query: any = {};
-
-    if (tags) {
-        query.tags = { $in: Array.isArray(tags) ? tags : [tags] };
-    }
-    
-    if (name) {
-        const nameString: string = name as string;
-        query.name = new RegExp(nameString, 'i');
-    }
-    
-    // Perform the search using the built query
-    const products = await Product.find(query);
-    
-    response.json(products);
-})
