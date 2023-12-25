91dba18faa6d6c8c5fe4b90f8c725f2d2bdaf311
Kunal gaur <kunal.gaur@simplyfi.tech>
2023-12-25 18:38:39 +0530

    Added new controllers

--- src/controller/order.ts
+++ src/controller/order.ts
@@ -4,6 +4,10 @@ import { OrderServices } from "../services/OrderServices";
 
 export const createOrder = errorHandler(async (request:Request, response: Response) => {
     const data = await OrderServices.createOrder(request.body);
+    return response.status(data.statusCode).json(data);
+});
 
+export const updateOrderPaymentStatus = errorHandler(async (request: Request, response: Response) => {
+    const data = await OrderServices.updateOrderStatus(request.params.id);
     return response.status(data.statusCode).json(data);
 });
