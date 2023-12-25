0c3e1d491e4d5e1d33779ffa1e999eceb3dea581
Kunal gaur <kunal.gaur@simplyfi.tech>
2023-12-25 18:39:48 +0530

    Removed a route

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { getAllProducts, getSingleProduct, searchProducts } from "../controller/product";
+import { getAllProducts, getSingleProduct } from "../controller/product";
 
 const router = Router();
 
 router.route('/').get(getAllProducts);
-router.route('/search').get(searchProducts);
 router.route('/:id').get(getSingleProduct);
 
 export default router;
